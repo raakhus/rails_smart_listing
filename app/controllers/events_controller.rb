9fcb0e5eab2fde130a564050ee12dc4f93d72819
Mark Stouffer <admin@thefullertonian.com>
2019-03-18 11:48:10 -0700

    copied the smart listing form live example and replaced @user with @event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,55 +2,44 @@ class EventsController < ApplicationController
   include SmartListing::Helper::ControllerExtensions
   helper  SmartListing::Helper
   def index
-    @time = Time.now
-    @events = Event.upcoming
+    smart_listing_create :events, 
+                         Event.active, 
+                         partial: "events/list", 
+                         default_sort: {name: "asc"}
   end
-  def show
-    @event = Event.find(params[:id])
-  end
-  def edit
-    @event = Event.find(params[:id])
-  end
-  def update
-    @event = Event.find(params[:id])
-    @event.update(event_params)
-    if @event.update(event_params)
-      flash[:notice] = "Event successfully updated"
-      redirect_to @event
-    else
-      render :new
-    end
-  end
+  
   def new
     @event = Event.new
   end
+  
   def create
-    @event = Event.new(event_params)
-   if @event.save
-    flash[:notice] = "Event successfully saved"
-     redirect_to @event
-   else
-     render :new
-   end
+    @event = Event.create(user_params)
   end
+  
+  def edit
+  end
+  
+  def update
+    @event.update_attributes(event_params)
+  end
+  
   def destroy
-    @event = Event.find(params[:id])
-   if @event.destroy
-    flash[:notice] = "Event successfully deleted"
-    redirect_to events_url
-   end
+    @event.destroy
   end
-  def previous
-    @events = Event.previous
-    @time = Time.now
+  
+  def credit_card_switch
+    @event.toggle :credit_card
   end
-  def all
-    @events = Event.all
-    @time = Time.now
-  end
+  
   private
-    def event_params
-      params.require(:event).permit(:name, :desciption, :location, :price, :starts_at, :image_file_name, :capacity)
-    end
+  
+  def find_user
+    @event = Event.find(params[:id])
+  end
+  
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :price, :starts_at, :image_file_name, :capacity)
+  end
 end
+
 
